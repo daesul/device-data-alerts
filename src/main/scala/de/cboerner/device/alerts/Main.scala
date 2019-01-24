fb021506f12f60e80cfb37b53e582476ff82ca32
cBoerner <#Chris250185!!>
2019-01-24 14:28:51 +0100

    added start up

--- src/main/scala/de/cboerner/device/alerts/Main.scala
+++ src/main/scala/de/cboerner/device/alerts/Main.scala
@@ -1,9 +1,12 @@
 package de.cboerner.device.alerts
 
+import akka.actor.ActorSystem
+
 object Main {
 
   def main(args: Array[String]): Unit = {
-
+    val system = ActorSystem("device-data-alerts")
+    val root = system.actorOf(Root.props(), "root")
   }
 
 }
